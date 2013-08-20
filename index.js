0f7fd1660a095970733751d40cf4fc22cf6b34a1
3rd-Eden <info@3rd-Eden.com>
2013-08-20 11:52:48 +0200

    [minor] Compilation adjustments

--- index.js
+++ index.js
@@ -17,16 +17,16 @@ var source = [
   // a.k.a. writing bad code in general. These variable declarations ensure that
   // we don't have horrible global leaks in our own code.
   //
-  'var EJSON, EJSONTest, i, base64Encode, base64Decode, root = this;',
+  'var EJSON, EJSONTest, i, base64Encode, base64Decode, root = {};',
 
   //
   // Add the required dependencies and include them as full source so we can
   // re-use this code for Node.js as well as on the client.
   //
-  fs.readFileSync(require.resolve('underscore'), 'utf-8'),
-  'var _ = "undefined" !== typeof exports ? exports._ : this._;',
-  fs.readFileSync('./vendor/ejson.js', 'utf-8'),
-  fs.readFileSync('./vendor/base64.js', 'utf-8'),
+  fs.readFileSync(require.resolve('underscore'), 'utf-8').slice(0, -7) + 'root);',
+  'var _ = "undefined" !== typeof exports ? exports._ : root._;',
+  fs.readFileSync(path.join(__dirname, './vendor/ejson.js'), 'utf-8'),
+  fs.readFileSync(path.join(__dirname, './vendor/base64.js'), 'utf-8'),
 
   '  root.EJSON = EJSON;',
   '}).call(this);'
@@ -43,7 +43,7 @@ var EJSON = load.compiler(source, 'EJSON.js', {
   Uint8Array: Uint8Array,   // Used for instanceof check in ejson.js
   Array: Array,             // Used for instanceof check in ejson.js
   Date: Date,               // Used for instanceof check in ejson.js
-}).EJSON;
+});
 
 //
 // Expose the source code.
