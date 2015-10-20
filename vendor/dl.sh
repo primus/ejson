9f081db0b1169f3295a3ef9a3407bb0b628631b3
Luigi Pinca <luigipinca@gmail.com>
2015-10-20 10:39:39 +0200

    [fix] Embed the stringify.js contents
    Fixes #13

--- vendor/dl.sh
+++ vendor/dl.sh
@@ -1,4 +1,9 @@
-rm ejson.js
-rm base64.js
-wget https://raw.githubusercontent.com/meteor/meteor/devel/packages/ejson/ejson.js
-wget https://raw.githubusercontent.com/meteor/meteor/devel/packages/base64/base64.js
+#!/usr/bin/env bash
+
+set -e
+
+BASE_URL=https://raw.githubusercontent.com/meteor/meteor/devel/packages
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+rm {base64,ejson,stringify}.js
+wget $BASE_URL/{base64/base64,ejson/{ejson,stringify}}.js
