8a9d17c9a80c6b640a0c8d7339971df458b601b6
Luigi Pinca <luigipinca@gmail.com>
2016-01-24 09:12:04 +0100

    [minor] Make rm ignore nonexistent files

--- vendor/dl.sh
+++ vendor/dl.sh
@@ -5,5 +5,5 @@ set -e
 BASE_URL=https://raw.githubusercontent.com/meteor/meteor/devel/packages
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
-rm {base64,ejson,stringify}.js
+rm -f {base64,ejson,stringify}.js
 wget $BASE_URL/{base64/base64,ejson/{ejson,stringify}}.js
