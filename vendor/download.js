f42f2e47df4233b02e381bf908019d9e3e7a4699
Luigi Pinca <luigipinca@gmail.com>
2019-05-09 15:59:45 +0200

    [ignore] Do not ignore vendor/{download,meteor}.js

---
+++ vendor/download.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const { get } = require('https');
+const { join } = require('path');
+const { promisify } = require('util');
+const { writeFile } = require('fs');
+
+const write = promisify(writeFile);
+
+/**
+ * Fetch a URL.
+ *
+ * @param {String} url The URL to fetch
+ * @return {Promise<String>} A Promise that resolves with the URL content
+ * @private
+ */
+function fetch(url) {
+  return new Promise((resolve, reject) => {
+    const request = get(url, (response) => {
+      if (response.statusCode !== 200) {
+        reject(new Error(`Unexpected status code: ${response.statusCode}`));
+        return;
+      }
+
+      const chunks = [];
+      let length = 0;
+
+      response.on('data', (chunk) => {
+        length += chunk.length;
+        chunks.push(chunk);
+      });
+
+      response.on('end', () => {
+        resolve(Buffer.concat(chunks, length).toString());
+      });
+    });
+
+    request.on('error', reject);
+  });
+}
+
+const baseUrl =
+  'https://raw.githubusercontent.com/meteor/meteor/devel/packages';
+
+Promise.all(
+  ['base64.js', 'ejson.js', 'stringify.js'].map(async (file, index) => {
+    const url = `${baseUrl}/${index === 0 ? 'base64' : 'ejson'}/${file}`;
+    await write(join(__dirname, file), await fetch(url));
+  })
+).catch((err) => {
+  console.error(err);
+  process.exitCode = 1;
+});
---
+++ vendor/meteor.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+  //
+  // When fibers are not supported on you system Meteor automatically sets this
+  // function to a nope function. We're going to do the same here as there are
+  // small parts of the code that call this function.
+  //
+  _noYieldsAllowed(f) {
+    return f();
+  }
+};
